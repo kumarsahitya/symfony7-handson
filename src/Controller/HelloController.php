3016910b7e0248ca01fe3aa8186d434ac2676fc4
kumarsahitya <ksahitya27@gmail.com>
2024-12-20 18:24:59 +0530

    0014-routing-using-attributes

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,16 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HelloController
 {
+    private array $greetings = [
+        'Hello',
+        'Hi',
+        'Bye!',
+    ];
+
     /**
      * Handles the request and returns a greeting response.
      *
      * @return Response A response object containing a greeting message.
      */
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return new Response("Hello!");
+        return new Response(implode(', ', $this->greetings));
+    }
+
+    /**
+     * Handles the request and returns a response with a specific greeting message.
+     *
+     * @param int|string $id The identifier for the greeting message to be returned.
+     * @return Response A response object containing the specified greeting message.
+     */
+    #[Route('/messages/{id}', name: 'app_show_one')]
+    public function showOne($id): Response
+    {
+        return new Response($this->greetings[$id]);
     }
 }
