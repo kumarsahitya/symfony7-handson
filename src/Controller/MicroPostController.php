74c581e2ece07975f69a768326cda19c1f53ec50
kumarsahitya <ksahitya27@gmail.com>
2024-12-28 11:45:23 +0530

    0058-project-getting-data-from-database

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,27 +2,27 @@
 
 namespace App\Controller;
 
-use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
-        dd($posts->findAll());
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
         ]);
     }
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
     public function showOne(MicroPost $post): Response
     {
-        dd($post);
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
+        ]);
     }
 }
