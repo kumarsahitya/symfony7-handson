0501ab3c82332f73cf9065d9557eefe568090f9c
kumarsahitya <ksahitya27@gmail.com>
2025-01-21 12:53:23 +0530

    0252-file-upload-form-twig

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\UserProfile;
+use App\Form\ProfileImageType;
 use App\Form\UserProfileType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -54,10 +55,12 @@ class SettingsProfileController extends AbstractController
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function profileImage(): Response
     {
+        $form = $this->createForm(ProfileImageType::class);
+
         return $this->render(
             'settings_profile/profile_image.html.twig',
             [
-                // 'form' => $form->createView(),
+                'form' => $form->createView(),
             ]
         );
     }
--- src/Form/ProfileImageType.php
+++ src/Form/ProfileImageType.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace App\Form;
+
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\File;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
 class ProfileImageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -26,6 +29,7 @@ class ProfileImageType extends AbstractType
                 ]
             ]);
     }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
